edbd9f8b98a1794850ac5e6b6468eeec3e58c75a
Matthew Tang <matttang27@gmail.com>
2022-01-12 16:23:33 -0500

    Some more improvements to MainView

--- src/main/java/org/vaadin/Generate.java
+++ src/main/java/org/vaadin/Generate.java
@@ -1,5 +1,6 @@
 package org.vaadin;
 import java.util.ArrayList;
+import java.util.Arrays;
 public class Generate {
     static String fromConstruct(String constructor) {
         String example = "public Task(String name, LocalDateTime created, LocalDateTime lastEdited, LocalDateTime nextDue, String cronJob, int priority, boolean done, Color color, String background, String icon, Task parent, String description, int id)";
@@ -22,6 +23,22 @@ public class Generate {
         }
         output += String.format("%s Example = new %s%s;",className,className,test.toString().replace("[",
         "(").replace("]",")"));
-        System.out.println(output);
+        return output;
+    }
+    static String fromList(String[][] list) {
+        String output = "";
+        String className = list[0][0];
+        output += String.format("System.out.println(\"Enter info for %s:\")\n",list[0][0]);
+        String construct = "";
+        for (int i=1;i<list.length;i++) {
+            String type = list[i][0];
+            String name = list[i][1];
+            output += String.format("System.out.println(\"Enter %s (Type %s):\")\n",name,type);
+            output += String.format("%s t%s = Scanner.nextLine();\n",type,name);
+            construct += String.format("%s %s, ");
+        }
+        construct = construct.substring(0,construct.length() - 2);
+        output += String.format("%s Example = new %s(%s);",className,className,construct);
+        return output;
     }
 }
--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.*;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.progressbar.ProgressBar;
@@ -12,6 +13,11 @@ import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.component.notification.Notification;
+import java.util.ArrayList;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.textfield.TextAreaVariant;
+
 @PageTitle("Java Tools")
 @Route("")
 
@@ -19,27 +25,56 @@ public class MainView extends VerticalLayout {
 
     public MainView() {
 
-
-
         VerticalLayout manual = new VerticalLayout();
-        VerticalLayout construct = new VerticalLayout();
 
         VerticalLayout fieldList = new VerticalLayout();
-        TextField nameField = new TextField();
-        nameField.setPlaceholder("Name");
+        ArrayList<TextField> names = new ArrayList<TextField>();
+        ArrayList<TextField> types = new ArrayList<TextField>();
         TextField typeField = new TextField();
         typeField.setPlaceholder("Type");
+        TextField nameField = new TextField();
+        nameField.setPlaceholder("Name");
+
         Button addButton = new Button("Add");
         addButton.addClickListener(click -> {
-            
-            Checkbox checkbox = new Checkbox(nameField.getValue());
-            fieldList.add(checkbox);
-            nameField.setValue("");
+            if (nameField.getValue().equals("") || typeField.getValue().equals("")) {
+                if (nameField.getValue().equals("") ) {
+                    nameField.getStyle().set("color", "red");
+                    
+                }
+                if (typeField.getValue().equals("") ) {
+                    typeField.getStyle().set("color", "red");
+                }
+                
+            } 
+            else {
+
+                TextField ntext = new TextField();
+                ntext.setValue(nameField.getValue());
+                ntext.setHeight("100");
+                names.add(ntext);
+                TextField ttext = new TextField();
+                ttext.setValue(typeField.getValue());
+                types.add(ttext);
+                Button removeButton = new Button("", VaadinIcon.CLOSE.create());
+                removeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY, ButtonVariant.LUMO_ERROR);
+                HorizontalLayout row = new HorizontalLayout(ttext, ntext, removeButton);
+                removeButton.addClickListener(click2 -> {
+                    fieldList.remove(row);
+                });
+                fieldList.add(row);
+                nameField.setValue("");
+                typeField.setValue("");
+            }
         });
         addButton.addClickShortcut(Key.ENTER);
-        HorizontalLayout manualInput = new HorizontalLayout(nameField,typeField,addButton);
-        manual.add(new H2("Manual Input"),fieldList,manualInput);
+        HorizontalLayout manualInput = new HorizontalLayout(typeField, nameField, addButton);
+        fieldList.add(manualInput);
+        manual.add(new H2("Manual Input"), fieldList);
 
+        // ----------------------------------------------
+
+        VerticalLayout construct = new VerticalLayout();
 
         TextField cField = new TextField();
         Button generate = new Button("Generate");
@@ -51,20 +86,30 @@ public class MainView extends VerticalLayout {
         pBarLabel.setText("Generating report...");
         pBar.setVisible(false);
         pBarLabel.setVisible(false);
-        
+
+        construct.add(new H2("Copy from constructor"), cField, generate, pBarLabel, pBar);
+
+        // ----------------------------------------------
+
+        VerticalLayout result = new VerticalLayout();
+        TextArea textArea = new TextArea("Result");
+
+        result.add(textArea);
+        result.setVisible(false);
+
         generate.addClickListener(click -> {
-            pBar.setVisible(true);
-            pBarLabel.setVisible(true);
+            // pBar.setVisible(true);
+            // pBarLabel.setVisible(true);
+            result.setVisible(true);
         });
-        
-        construct.add(new H2("Copy from constructor"),cField,generate,pBarLabel, pBar);
+
         add(
-            new H1("Java Scanner Generator"),
-            new HorizontalLayout(
-                manual,
-                construct
-            )
-            
+                new H1("Java Scanner Generator"),
+                new HorizontalLayout(
+                        manual,
+                        construct),
+                result
+
         );
     }
 }
